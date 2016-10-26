32e7f0adff4ddc9548452977922b2d5bbb71bf01
Gleb Stiblo <errandir@gmail.com>
2016-10-26 11:31:30 +0600

    try to reduce memory use

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import {
-  AsyncStorage
+	AsyncStorage
 } from 'react-native'
 
 import fs from 'react-native-fs'
@@ -12,7 +12,7 @@ const PLACEHOLDER = '~'
 let CREATE_ROOT_DIR = fs.mkdir(ROOT_DIR);
 
 module.exports = createInstance()
-exports.THRESHOLD_SIZE = 65000;
+exports.THRESHOLD_SIZE = 32000;
 
 /**
  * get singleton with AsyncStorage API, which uses
@@ -21,131 +21,126 @@ exports.THRESHOLD_SIZE = 65000;
  *
  * @return {instance}
  */
-function createInstance () {
-  let fsStorage = getFSStorage()
+function createInstance() {
+	let fsStorage = getFSStorage()
 
-  return {
-    getItem,
-    setItem,
-    removeItem,
-    clear,
-    multiGet,
-    multiSet,
-    multiRemove,
-    mergeItem: notImplemented,
-    multiMerge: notImplemented,
-    getAllKeys: AsyncStorage.getAllKeys,
-  }
+	return {
+		getItem,
+		setItem,
+		removeItem,
+		clear,
+		multiGet,
+		multiSet,
+		multiRemove,
+		mergeItem: notImplemented,
+		multiMerge: notImplemented,
+		getAllKeys: AsyncStorage.getAllKeys,
+	}
 
-  function setItem (key, value) {
-    return multiSet([[key, value]])
-  }
+	function setItem(key, value) {
+		return multiSet([[key, value]])
+	}
 
-  function removeItem (key) {
-    return multiRemove([key])
-  }
+	function removeItem(key) {
+		return multiRemove([key])
+	}
 
-  function getItem (key) {
-    return multiGet([key])
-      .then(pairs => {
-        const val = pairs[0] ? pairs[0][1] : null
-        if (val == null) throw new Error('NotFound')
+	function getItem(key) {
+		return multiGet([key])
+			.then(pairs => {
+				const val = pairs[0] ? pairs[0][1] : null
+				if (val == null) throw new Error('NotFound')
 
-        return val
-      })
-  }
+				return val
+			})
+	}
 
-  function clear () {
-    return Promise.all([
-      AsyncStorage.clear(),
-      fsStorage.clear()
-    ])
-  }
+	function clear() {
+		return Promise.all([
+			AsyncStorage.clear(),
+			fsStorage.clear()
+		])
+	}
 
-  function multiRemove (keys) {
-    return Promise.all(
-      AsyncStorage.multiRemove(keys),
-      fsStorage.multiRemove(keys)
-    )
-  }
+	function multiRemove(keys) {
+		return Promise.all(
+			AsyncStorage.multiRemove(keys),
+			fsStorage.multiRemove(keys)
+		)
+	}
 
-  function multiGet (keys) {
-    return AsyncStorage.multiGet(keys)
-      .then(pairs => {
-        const inFS = pairs.filter(([k, v]) => v === PLACEHOLDER)
-          .map(pair => pair[0])
+	function multiGet(keys) {
+		return AsyncStorage.multiGet(keys).then(pairs => {
+			const inFS = pairs.filter(([k, v]) => v === PLACEHOLDER).map(pair => pair[0]);
+			return inFS.length ? lookupInFSAndMerge(pairs, inFS) : pairs;
+		});
+	}
 
-        return inFS.length ? lookupInFSAndMerge(pairs, inFS) : pairs
-      })
-  }
+	function lookupInFSAndMerge(pairs, inFS) {
+		// return fsStorage.multiGet(pairs.map(pair => pair[0]))
+		return fsStorage.multiGet(inFS)
+			.then(vals => {
+				let j = 0;
+				for (var i = 0; i < pairs.length; i++) {
+					const pair = pairs[i];
+					if (pair[1] === PLACEHOLDER) {
+						pairs[i] = [pair[0], vals[j++]];
+					}
+				}
+				return pairs;
+			})
+	}
 
-  function lookupInFSAndMerge (pairs, inFS) {
-    // return fsStorage.multiGet(pairs.map(pair => pair[0]))
-    return fsStorage.multiGet(inFS)
-      .then(vals => {
-        return pairs.map(pair => {
-          return pair[1] === PLACEHOLDER
-            ? [pair[0], vals.shift()]
-            : pair
-        })
-      })
-  }
+	function multiSet(pairs) {
+		// pairs = pairs.slice()
+		const inFileSystem = [];
+		for (var i = 0; i < pairs.length; i++) {
+			const pair = pairs[i];
+			if ((pair[1].length > exports.THRESHOLD_SIZE)) {
+				inFileSystem.push([pair[0], pair[1]]);
+				pair[1] = PLACEHOLDER;
+			}
+		}
 
-  function multiSet (pairs) {
-    pairs = pairs.slice()
-    const inFileSystem = []
-    for (var i = 0; i < pairs.length; i++) {
-      const pair = pairs[i];
-      const toFs = willPutInFS(pair);
-      console.log('ZAZAZAZAZA:', pair[0], (pair[1]+'').length, toFs);
-      if (toFs) {
-        inFileSystem.push(pair.slice())
-        pair[1] = PLACEHOLDER
-      }
-    }
+		return Promise.all([AsyncStorage.multiSet(pairs), fsStorage.multiSet(inFileSystem)]);
+	}
 
-    return Promise.all([
-      AsyncStorage.multiSet(pairs),
-      fsStorage.multiSet(inFileSystem)
-    ])
-  }
+	function getFSStorage() {
+		return utils.postponeMethods({
+			clear: fsClear,
+			multiRemove: fsMultiRemove,
+			multiGet: fsMultiGet,
+			multiSet: fsMultiSet
+		}, CREATE_ROOT_DIR)
+	}
 
-  function getFSStorage () {
-    return utils.postponeMethods({
-      clear: fsClear,
-      multiRemove: fsMultiRemove,
-      multiGet: fsMultiGet,
-      multiSet: fsMultiSet
-    }, CREATE_ROOT_DIR)
-  }
+	function fsClear() {
+		CREATE_ROOT_DIR = fs.unlink(ROOT_DIR).then(() => fs.mkdir(ROOT_DIR))
+		fsStorage = getFSStorage()
+		return CREATE_ROOT_DIR
+	}
 
-  function fsClear () {
-    CREATE_ROOT_DIR = fs.unlink(ROOT_DIR).then(() => fs.mkdir(ROOT_DIR))
-    fsStorage = getFSStorage()
-    return CREATE_ROOT_DIR
-  }
+	function fsMultiRemove(keys) {
+		return utils.allSettled(pairs.map(([key, val]) => fs.unlink(toFSKey(key), val)))
+	}
 
-  function fsMultiRemove (keys) {
-    return utils.allSettled(pairs.map(([key, val]) => fs.unlink(toFSKey(key), val)))
-  }
+	function fsMultiSet(pairs) {
+		return Promise.all(pairs.map(([key, val]) => fs.writeFile(toFSKey(key), val)))
+	}
 
-  function fsMultiSet (pairs) {
-    return Promise.all(pairs.map(([key, val]) => fs.writeFile(toFSKey(key), val)))
-  }
+	function fsMultiGet(keys) {
+		return utils.allSettled(keys.map(k => fs.readFile(toFSKey(k))))
+	}
 
-  function fsMultiGet (keys) {
-    return utils.allSettled(keys.map(k => fs.readFile(toFSKey(k))))
-  }
+	function toFSKey(key) {
+		return `${ROOT_DIR}/${hash(key)}`
+	}
 
-  function toFSKey (key) {
-    return `${ROOT_DIR}/${hash(key)}`
-  }
-
-  function willPutInFS ([key, value]) {
-    return value.length > exports.THRESHOLD_SIZE
-  }
+	function willPutInFS([key, value]) {
+		return value.length > exports.THRESHOLD_SIZE
+	}
 }
 
-function notImplemented () {
-  throw new Error('not implemented')
+function notImplemented() {
+	throw new Error('not implemented')
 }
